09bd946134355c50b61864c1b67b934469b83da6
Anthony Chu <anthony@anthonychu.ca>
2021-04-03 15:27:07 -0700

    Get custom handler working

--- graphql/index.js
+++ graphql/index.js
@@ -1,15 +1,16 @@
-const fetch = require('sync-fetch')
+const fetch = require('sync-fetch');
 
-const jsonGraphqlExpress = require('json-graphql-server');
+const jsonGraphqlExpress = require('json-graphql-server').default;
 
 
 const data = fetch('https://github.com/bketelsen/bkml/releases/download/blox/data.json').json();
 const app = require('express')();
 
 
-app.use('/api/graphql', jsonGraphqlExpress(data));
+app.use('/', jsonGraphqlExpress(data));
 console.log(data);
 
-const PORT = process.env.FUNCTIONS_HTTPWORKER_PORT;
+// FUNCTIONS_HTTPWORKER_PORT still works, but guidance is to use FUNCTIONS_CUSTOMHANDLER_PORT
+const port = process.env.FUNCTIONS_CUSTOMHANDLER_PORT;
 
-module.exports = app.listen(PORT, () => console.log(`Server running on ${port}, http://localhost:${port}`));
+module.exports = app.listen(port, () => console.log(`Server running on ${port}, http://localhost:${port}`));
